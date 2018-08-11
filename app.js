6a16554100cbd5be7108a385e1196a42e63a8310
hyeonson <gumgim95@naver.com>
2018-08-11 17:17:02 +0900

    0811

--- app.js
+++ app.js
@@ -66,30 +66,34 @@ app.post('/signup', function (req, res){
   req.on('end', function(){
     conole.log('noting here');
   });
-  var user_id = inputData.user_id;
-  var user_pw = inputData.user_pw;
-  var user_name = inputData.user_name;
-  var user_age = inputData.user_age;
-  var user_saying = inputData.user_saying;
-  var user_major = inputData.user_major;
-  var user_sex = inputData.user_sex;
-  var user_grade = inputData.user_grade;
-  var user = new User({user_id:user_id, user_pw:user_pw, user_nama:user_name, user_age:user_age, user_saying:user_saying,
-  user_major:user_major, user_sex:user_sex, user_grade:user_grade})
+  if(typeof(user_id) == 'undefined'){
+    //err
+  } else {
+    var user_id = inputData.user_id;
+    var user_pw = inputData.user_pw;
+    var user_name = inputData.user_name;
+    var user_age = inputData.user_age;
+    var user_saying = inputData.user_saying;
+    var user_major = inputData.user_major;
+    var user_sex = inputData.user_sex;
+    var user_grade = inputData.user_grade;
+    var user = new User({user_id:user_id, user_pw:user_pw, user_nama:user_name, user_age:user_age, user_saying:user_saying,
+    user_major:user_major, user_sex:user_sex, user_grade:user_grade})
 
-  console.log('user_id: ' + user_id);
-  console.log('user_pw: ' + user_pw);
-  console.log('user_name: ' + user_name);
-  console.log('user_age: ' + user_age);
-  console.log('user_saying: ' + user_saying);
-  console.log('user_major: ' + user_major); 
-  console.log('user_sex: ' + user_sex);
-  console.log('user_grade: ' + user_grade);
-  user.save(function(err){
-    if (err) console.log(err);
-    res.write('111');
-    res.end();
-  });
+    console.log('user_id: ' + user_id);
+    console.log('user_pw: ' + user_pw);
+    console.log('user_name: ' + user_name);
+    console.log('user_age: ' + user_age);
+    console.log('user_saying: ' + user_saying);
+    console.log('user_major: ' + user_major); 
+    console.log('user_sex: ' + user_sex);
+    console.log('user_grade: ' + user_grade);
+    user.save(function(err){
+      if (err) console.log(err);
+      res.write('111');
+      res.end();
+    });
+  }
 });
 
 //Express 서버 시작
